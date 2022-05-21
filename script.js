8499e83f5a415f411212e93f47434965e7216535
ellionardo <elionars@gmail.com.br>
2022-05-21 17:19:40 -0300

    validador correções

--- script.js
+++ script.js
@@ -2,21 +2,26 @@ console.log('Java script carregado');
 
 function validaCPF(cpf) {
     if(cpf.length != 11) {
-        return false;
+        return false;        
     } else {
+
         var numeros = cpf.substring(0, 9);
         var digitos = cpf.substring(9);
         
         var soma = 0;
-        for (i=10; i>1; i--) {
-            
+        for(var i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i;
+            console.log(soma)
         }
+
     }
 }
 
 function validacao() {
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'none';
+
     var cpf = document.getElementById('cpf_digitado').value;
-    console.log(cpf);
     
     var resultadoValidacao = validaCPF(cpf);
 
